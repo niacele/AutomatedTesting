311ae8ce5e6cf0230a6ce3f5622dbd9a74fcdbfb
Nia <161420660+niacele@users.noreply.github.com>
2024-05-17 12:39:18 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/automatin_unit_testing_group_one/Automatin_Unit_Testing_Group_One.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automatin_unit_testing_group_one;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automatin_Unit_Testing_Group_One {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/automatin_unit_testing_group_one/UnitTestingTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.automatin_unit_testing_group_one;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class UnitTestingTest {
+    
+    public UnitTestingTest() {
+    }
+
+    @Test
+    public void testAddition() {
+    }
+
+    @Test
+    public void testSubtration() {
+    }
+
+    @Test
+    public void testMessage() {
+    }
+    
+}
